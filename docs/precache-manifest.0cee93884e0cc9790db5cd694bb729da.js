3c4c17ab15beefc76ac19c0290bc9becf44778f9
uznetro <mayanomikotoba@gmail.com>
2019-07-17 10:39:24 +0900

    Add files via upload

---
+++ docs/precache-manifest.0cee93884e0cc9790db5cd694bb729da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "5a21d05320120acd2aa7",
+    "url": "js/chunk-vendors.c4e5907b.js"
+  },
+  {
+    "revision": "e4c2320aab6489e80272",
+    "url": "js/app.0ee23568.js"
+  },
+  {
+    "revision": "af731fcf9bff0d637ed02300b6e814a2",
+    "url": "index.html"
+  },
+  {
+    "revision": "5055d4d186bf8feaa9f7425a3e65cd37",
+    "url": "img/pwva-logo.5055d4d1.png"
+  },
+  {
+    "revision": "e4c2320aab6489e80272",
+    "url": "css/app.ddf2999c.css"
+  }
+];
